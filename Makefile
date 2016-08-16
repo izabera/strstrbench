9c0e448c1b38f198f6fd1946617e3622ff617730
izabera <izaberina@gmail.com>
2016-08-16 20:00:04 +0200

    fasdfasd

--- Makefile
+++ Makefile
@@ -1,10 +1,13 @@
-all:
-	rm *.o
-	$(CC) $(CFLAGS) antiopt.c -c -o antiopt.o
-	$(CC) $(CFLAGS) uclibc.c -c -o uclibc.o
-	$(CC) $(CFLAGS) mine.c -c -o mine.o
-	$(CC) $(CFLAGS) musl.c -c -o musl.o
-	$(CC) $(CFLAGS) fast.c -c -o fast.o
-	$(CC) $(CFLAGS) diet.c -c -o diet.o
-	$(CC) $(CFLAGS) bsd.c -c -o bsd.o
+CC = gcc
+CFLAGS = -Ofast -march=native -Wall -Wextra
+LDFLAGS = -flto
+
+.PHONY: all
+
+%.o: %.c
+	$(CC) $(CFLAGS) -c -o $@ $<
+
+all: bench
+
+bench: antiopt.o uclibc.o plan9.o mine.o musl.o fast.o diet.o bsd.o bench.c Makefile
 	$(CC) $(CFLAGS) $(LDFLAGS) bench.c *.o -o bench
--- bench.c
+++ bench.c
@@ -14,23 +14,29 @@ char *diet_strstr(const char *, const char *);
 char *musl_strstr(const char *, const char *);
 char *my_strstr(const char *, const char *);
 char *fast_strstr(const char *, const char *);
+char *plan9_strstr(const char *, const char *);
 
 void stupid_gcc_stop_optimizing_this_away_for_fucks_sake(const char *);
 
 int main() {
   struct { char *word, *filename; } tests[] = {
-    { "smoothness"        , "texts/anna-karenina.txt"                          }, // line 10003
-    { "reglez"            , "texts/artamene.txt"                               }, // line 10010
-    { "wears"             , "texts/brothers-karamazov.txt"                     }, // line 10001
-    { "hark"              , "texts/complete-shakespear.txt"                    }, // line 10006
-    { "Pride"             , "texts/iliad.txt"                                  }, // line 10002
-    { "Haggerston"        , "texts/pride-and-prejudice.txt"                    }, // line 10006
-    { "abiding"           , "texts/republic.txt"                               }, // line 10016
-    { "disorder"          , "texts/war-and-peace.txt"                          }, // line 10012
+    { "smoothness"           , "tests/texts/anna-karenina.txt"       }, // line 10003
+    { "reglez"               , "tests/texts/artamene.txt"            }, // line 10010
+    { "wears"                , "tests/texts/brothers-karamazov.txt"  }, // line 10001
+    { "hark"                 , "tests/texts/complete-shakespear.txt" }, // line 10006
+    { "Pride"                , "tests/texts/iliad.txt"               }, // line 10002
+    { "Haggerston"           , "tests/texts/pride-and-prejudice.txt" }, // line 10006
+    { "abiding"              , "tests/texts/republic.txt"            }, // line 10016
+    { "disorder"             , "tests/texts/war-and-peace.txt"       }, // line 10012
+    { "ACAGCGACATACCGATATGC" , "tests/dna/100k"                      },
+    { "GCGTCCTGCTGGGTCAAACG" , "tests/dna/1M"                        },
+    { "GCAACGAACCCCAGGAGCTC" , "tests/dna/10M"                       },
+    { "aaaaaaaaaaaaaaaaaaaa" , "tests/repetitions/20-1"              },
   };
 
   struct { char *name; char *(*func)(const char *, const char *); } strstrs[] = {
     { "glibc"  ,         strstr },
+    { "plan9"  ,   plan9_strstr },
     { "musl"   ,    musl_strstr },
     { "uclibc" ,  uclibc_strstr },
     { "bsd"    ,     bsd_strstr },
@@ -55,7 +61,7 @@ int main() {
       for (int q = 0; q < 100; q++) 
         stupid_gcc_stop_optimizing_this_away_for_fucks_sake(strstrs[j].func(map, tests[i].word));
       gettimeofday(&t2, NULL);
-      printf("%-10s %f\n", strstrs[j].name,
+      printf("%-10s %11.6f\n", strstrs[j].name,
           (double) (t2.tv_sec  - t1.tv_sec) +
           (double) (t2.tv_usec - t1.tv_usec) / 1000000);
     }
